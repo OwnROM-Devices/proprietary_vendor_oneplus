3821a316e75d487e7649e6aa4b52204d1740d388
joshuous <joshuous@gmail.com>
2017-03-20 06:52:49 +0000

    Revert "BoostFramework blobs"
    This reverts commit b36d57c7ec020d1277e9cba1b85652175e57832b.

--- oneplus3/oneplus3-vendor.mk
+++ oneplus3/oneplus3-vendor.mk
@@ -836,12 +836,6 @@ PRODUCT_PACKAGES += \
     imssettings \
     qcnvitems \
     qcrilhook
-
-# Perf
-PRODUCT_PACKAGES += \
-    com.qualcomm.qti.Performance.xml \
-    libqti_performance \
-    QPerformance
 endif
 
 -include vendor/extra/devices.mk
