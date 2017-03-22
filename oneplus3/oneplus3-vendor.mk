4141dbbd119ab13525d117c51bdfee8600c6c5ba
joshuous <joshuous@gmail.com>
2017-03-22 10:38:42 +0000

    Import BoostFramework blobs

--- oneplus3/oneplus3-vendor.mk
+++ oneplus3/oneplus3-vendor.mk
@@ -836,6 +836,12 @@ PRODUCT_PACKAGES += \
     imssettings \
     qcnvitems \
     qcrilhook
+
+# Perf
+PRODUCT_PACKAGES += \
+    com.qualcomm.qti.Performance.xml \
+    libqti_performance \
+    QPerformance
 endif
 
 -include vendor/extra/devices.mk
